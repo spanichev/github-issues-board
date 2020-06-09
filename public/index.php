c4acceeda4adc9e3e9fdb436b1219e88c9036646
Sergiy Panichev <s.panichev@gmail.com>
2020-06-10 00:01:35 +0300

    Refactor index.php file

--- public/index.php
+++ public/index.php
@@ -3,22 +3,22 @@ require __DIR__.'/../vendor/autoload.php';
 
 use App\KanbanBoard\Application;
 use App\KanbanBoard\Authentication;
-use App\KanbanBoard\GithubClient;
+use App\KanbanBoard\GithubService;
 use App\Support\Env;
-use App\Utilities;
 use Dotenv\Dotenv;
 
 // Loading .env file
 Dotenv::create(Env::getRepository(), __DIR__.'/../', ['.env'])->load();
 
-$repositories = explode('|', Utilities::env('GH_REPOSITORIES'));
 $authentication = new Authentication();
 $token = $authentication->login();
-$github = new GithubClient($token, Utilities::env('GH_ACCOUNT'));
 
-$board = new Application($github, $repositories, array('waiting-for-feedback'));
+$board = new Application(new GithubService($token));
 $data = $board->board();
+
+//var_dump($data);
+
 $m = new Mustache_Engine(array(
-	'loader' => new Mustache_Loader_FilesystemLoader('../views'),
+	'loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/../src/views'),
 ));
 echo $m->render('index', array('milestones' => $data));
