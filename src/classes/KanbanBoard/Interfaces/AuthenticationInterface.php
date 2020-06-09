0717fdf6587c88a5ed57d0eeeafacbd9b7e14dc9
Sergiy Panichev <s.panichev@gmail.com>
2020-06-09 18:07:37 +0300

    Add interface documentation

--- src/classes/KanbanBoard/Interfaces/AuthenticationInterface.php
+++ src/classes/KanbanBoard/Interfaces/AuthenticationInterface.php
@@ -1,10 +1,21 @@
-<?php
-
+<?php declare(strict_types=1);
 
 namespace App\KanbanBoard\Interfaces;
 
 
 interface AuthenticationInterface
 {
+    /**
+     * Login method implementation
+     *
+     * @return string access_token
+     */
+    public function login(): string;
 
+    /**
+     * Logout method implementation
+     *
+     * @return void
+     */
+    public function logout(): void;
 }
